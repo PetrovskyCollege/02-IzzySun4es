439b32b51c5ccd9e361c8855a9388510faad10ea
Alexandra Chugaenko <sascha281201@gmail.com>
2023-11-01 10:48:25 +0300

    1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php">
+        <label for="email">Почта</label>
+        <input type="email" id="email">
+        <label for="password">Пароль</label>
+        <input type="password" id="password">
+        <button type="submit">Отправить</button>
+    </form>
+</body>
+</html>
